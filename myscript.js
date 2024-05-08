116fa17ab3703442f4e8e0e2139d8cbac35c0b28
CodeKiro <kirofiniello@outlook.it>
2024-05-09 01:29:28 +0200

    creo 3 variabili diverse per prendere in input i 3 parametri, e poi li concateno per visualizzarli in console

---
+++ myscript.js
@@ -0,0 +1,4 @@
+let getName = prompt('Qual è il tuo nome?');
+let getSurname = prompt('Qual è il tuo cognome?');
+let getColor = prompt('Qual è il tuo colore preferito?');
+console.log(getName + getSurname + getColor);
